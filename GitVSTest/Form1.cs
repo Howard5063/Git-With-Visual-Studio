ee30664a839b57faeb157c4598fe78aaaa88706f
Howard <howard5063@gmail.com>
2019-06-24 16:46:37 +0800

    第一次異動並推送

--- GitVSTest/Form1.cs
+++ GitVSTest/Form1.cs
@@ -16,5 +16,10 @@ namespace GitVSTest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("第一次異動並推送");
+        }
     }
 }
