2b20aa05f0cb3f6b782118dba50263ac1412567b
Howard <howard5063@gmail.com>
2019-06-24 17:58:28 +0800

    ver1

--- GitVSTest/Form1.cs
+++ GitVSTest/Form1.cs
@@ -19,11 +19,16 @@ namespace GitVSTest
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("第一次異動並推送");
-            MessageBox.Show("下載GitHub上的專案，第二次異動並推送");
-            MessageBox.Show("模擬合作夥伴同時進行修改");
-            MessageBox.Show("模擬合作同時進行修改，此時並沒有抓取到最新的版本，進行第三次異動並推送，會出現衝突");
-            MessageBox.Show("解決衝突後，進行第四次推送");
+            //MessageBox.Show("第一次異動並推送");
+            //MessageBox.Show("下載GitHub上的專案，第二次異動並推送");
+            //MessageBox.Show("模擬合作夥伴同時進行修改");
+            //MessageBox.Show("模擬合作同時進行修改，此時並沒有抓取到最新的版本，進行第三次異動並推送，會出現衝突");
+            //MessageBox.Show("解決衝突後，進行第四次推送");
+            //MessageBox.Show("回復上一次commit狀態");
+
+            MessageBox.Show("ver1");
+
+
         }
     }
 }
